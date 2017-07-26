991719bbcf7525d12cfdd3bc168ae96998fd872e
Jase Cutler <jasecutler@gmail.com>
2017-07-26 15:00:23 -0700

    Completed most of part three

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const path = require('path')
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const http = require('http')
+const os = require('os')
+const fs = require('fs')
+const Busboy = require('busboy')
+
+//Storing the user info
 const userInfo = {}
 
 app.engine('mustache', mustacheExpress());
@@ -18,7 +24,31 @@ app.use(expressValidator())
 app.get("/", function(req, res, next){
   res.render("index", {appType:"Express"})
 })
-
+app.get("/profileupload", function(req,res,next){
+	res.render("uploadpic")
+})
+app.post("/user/upload",function(req,res,next){
+	const busboy = new Busboy({ headers: req.headers })
+	console.log("Start of upload")
+	busboy.on('file',function(fieldname, file, filename, encoding, mimetype){
+		let saveTo = path.join('./static/uploads/', path.basename(filename));
+		userInfo.avatar = filename
+	 // Save the incoming file.
+	 console.log("File is uploading")
+     file.pipe(fs.createWriteStream(saveTo))
+	})
+	busboy.on('finish', function() {
+		 console.log('Upload complete');
+  		// Send back HTTP response.
+  		res.writeHead(200, { 'Connection': 'close' });
+		  // End response.
+		res.redirect('/info')
+	});
+	//Parse HTTP-POST upload
+	console.log("function registered")
+	return req.pipe(busboy);
+	
+})
 app.post("/user/input", function(req,res,next){
 	console.log(req.body)
 	req.checkBody('name',"error give me yo name").isLength(0,100)
@@ -40,12 +70,13 @@ app.post("/user/input", function(req,res,next){
 		}else {
 			userInfo.name = req.body.name
 			userInfo.email = req.body.email
-			res.redirect('/info')
+			res.redirect('/profileupload')
 		}
 })
 app.get("/info", function(req,res,next){
 	let html = `<h1>${userInfo.name}</h1><br>
-				<h2>${userInfo.email}</h2>`
+				<h2>${userInfo.email}</h2>
+				<img src="./static/uploads/${userInfo.avatar} />`
 	res.send(html)
 })
 app.listen(3000, function(){
